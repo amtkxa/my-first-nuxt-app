982ffc1b7b558017b2342ee04f07baff1d188ba1
amtkxa <scarlet0211@outlook.jp>
2019-02-02 20:41:45 +0900

    動的ルーティングを行なった場合に id を取得して表示するよう修正

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    <p>/users/_id.vue</p>
+    <p>User ID: {{userId}}</p>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      userId: this.$route.params.id
+    };
+  }
+};
+</script>
