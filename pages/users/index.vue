ea4da08bc0cb9f41298c9f4d469a5acccc69439f
amtkxa <scarlet0211@outlook.jp>
2019-02-03 10:40:58 +0900

    Qiita API から、Nuxt.js のタグが付けられた投稿を取得するよう修正

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -3,3 +3,20 @@
     <p>/users/index.vue</p>
   </div>
 </template>
+
+<script>
+export default {
+  async mounted() {
+    console.log(
+      JSON.stringify(
+        // Qiita API から、Nuxt.js のタグが付けられた投稿を取得する。
+        await this.$axios.$get(
+          "https://qiita.com/api/v2/items?query=tag:nuxt.js"
+        ),
+        true,
+        " "
+      )
+    );
+  }
+};
+</script>
